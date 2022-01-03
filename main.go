9dfe0a3bf05d08f3b36e39456a18cc7505c27e32
Chetan Sarva <chetan@pixelcop.net>
2022-01-03 14:04:49 -0500

    feat: add cli flags to disable total or delta times

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"time"
 )
 
+var showElapsed = true
+var showDelta = true
+
+func parseFlags() {
+	se := flag.Bool("no-elapsed", false, "Do not print the absolute elapsed time")
+	sd := flag.Bool("no-delta", false, "Do not print the delta elapsed time")
+	flag.Parse()
+
+	if se != nil && *se {
+		showElapsed = false
+	}
+	if sd != nil && *sd {
+		showDelta = false
+	}
+}
+
 func main() {
+	parseFlags()
+	if !(showElapsed || showDelta) {
+		fmt.Println("error: must enable either elapsed or delta")
+		os.Exit(1)
+	}
 	stat, _ := os.Stdin.Stat()
 	if (stat.Mode() & os.ModeCharDevice) != 0 {
 		fmt.Println("error: stdin not connected to a pipe")
 		os.Exit(1)
 	}
+
+	f := "["
+	if showElapsed {
+		f += "%-7s"
+	}
+	if showDelta {
+		if showElapsed {
+			f += " "
+		}
+		f += "+%-7s"
+	}
+	f += "] %s"
 
 	reader := bufio.NewReader(os.Stdin)
 	start := time.Now()
@@ -22,11 +56,30 @@ func main() {
 		if err != nil {
 			return
 		}
-		elapsed := time.Since(start).Round(1 * time.Microsecond).Milliseconds()
-		elapsedS := fmt.Sprintf("%dms", elapsed)
-		delta := time.Since(last).Round(1 * time.Microsecond).Milliseconds()
-		deltaS := fmt.Sprintf("%dms", delta)
-		fmt.Printf("[%-6s +%-6s] %s", elapsedS, deltaS, line)
-		last = time.Now()
+
+		var args []interface{}
+
+		if showElapsed {
+			// absolute elapsed time
+			elapsed := time.Since(start).Round(1 * time.Microsecond).Milliseconds()
+			elapsedS := fmt.Sprintf("%dms", elapsed)
+			args = append(args, elapsedS)
+		}
+
+		if showDelta {
+			// delta between last line
+			delta := time.Since(last).Round(1 * time.Microsecond).Milliseconds()
+			deltaS := fmt.Sprintf("%dms", delta)
+			args = append(args, deltaS)
+		}
+
+		args = append(args, line)
+		// "[%-7s +%-7s] %s"
+		fmt.Printf(f, args...)
+
+		if showDelta {
+			// update last line ts
+			last = time.Now()
+		}
 	}
 }
